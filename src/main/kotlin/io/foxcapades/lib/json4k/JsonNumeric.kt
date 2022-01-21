3881d7c4daf89b92fb30a9825cbee33da221b197
Elizabeth <foxcapade@gmail.com>
2022-01-21 09:56:20 -0500

    add JsonNumeric.isIntegral indicator

--- src/main/kotlin/io/foxcapades/lib/json4k/JsonNumeric.kt
+++ src/main/kotlin/io/foxcapades/lib/json4k/JsonNumeric.kt
@@ -6,5 +6,11 @@ package io.foxcapades.lib.json4k
  * @since v1.0.0
  */
 sealed interface JsonNumeric : JsonElement {
+
+  /**
+   * Indicates whether the numeric value is an integral number.
+   */
+  val isIntegral: Boolean
+
   class PrecisionLossException(msg: String) : RuntimeException(msg)
 }
